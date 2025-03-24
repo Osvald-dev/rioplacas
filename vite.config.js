6a7c715661db45065651a2bee3882d974e09ec0d
Osvaldo <ocasabella98@gmail.com>
2025-03-24 16:20:25 -0300

    chachacha

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rioplacas/',  // ⚠️ Reemplaza con el nombre de tu repo
+  base: '/rioplacas/',  
 });
