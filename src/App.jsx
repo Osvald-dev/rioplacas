20bf47dea611d4d7d1834c9c8c73aed61d274e71
Osvaldo <ocasabella98@gmail.com>
2025-03-24 21:55:14 -0300

    fix githubpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import  Contacto from "./pages/Contacto.jsx";
-import Home from './pages/Home.jsx';
-import Productos from './pages/Productos.jsx';
-import Nosotros from './pages/Nosotros.jsx';
-import Servicios from './pages/Servicios.jsx';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Contacto from "./pages/Contacto.jsx";
+import Home from "./pages/Home.jsx";
+import Productos from "./pages/Productos.jsx";
+import Nosotros from "./pages/Nosotros.jsx";
+import Servicios from "./pages/Servicios.jsx";
 import Categoria from "./pages/Categoria.jsx";
 import WhatsAppButton from "./components/WhatsappButton.jsx";
 
 const App = () => {
   return (
-    <BrowserRouter basename="/rioplacas">
-    <Router>
+    <HashRouter>  {/* Usa HashRouter en vez de BrowserRouter */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/productos" element={<Productos />} />
         <Route path="/productos/:categoria" element={<Categoria />} />
-
         <Route path="/contacto" element={<Contacto />} />
         <Route path="/nosotros" element={<Nosotros />} />
         <Route path="/servicios" element={<Servicios />} />
       </Routes>
-      
-      <WhatsAppButton /> 
-    </Router>
-    </BrowserRouter>
+      <WhatsAppButton />
+    </HashRouter>
   );
 };
 
